0ed73d2d77f041f69e1fc1ebb4dfdc24e46c8b7d
alvinlukmanNH <galinx20gmail.com>
2018-09-23 19:44:37 +0700

    Teks

---
+++ src/if3/pkg10117095/latihan9/teks/IF310117095Latihan9Teks.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if3.pkg10117095.latihan9.teks;
+
+/**
+ *
+ * @author 
+ * NAMA         : Alvin Lukman Nulhakim
+ * KELAS        : IF-3
+ * NIM          : 10117095
+ * Deklarasi    : Program ini berisi contoh penggunaan type data char
+ */
+public class IF310117095Latihan9Teks {
+    
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+        
+        
+    }
+    
+}
